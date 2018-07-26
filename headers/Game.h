84a4251c2a78db42afaf9ec2553299c895308487
Austin Roa <ausroa@users.noreply.github.com>
2018-07-26 00:04:21 -0400

    First commit

---
+++ headers/Game.h
@@ -0,0 +1,14 @@
+//
+// Created by Austin Roa on 7/26/18.
+//
+
+#ifndef C_CONSOLE_GAME_GAME_H
+#define C_CONSOLE_GAME_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //C_CONSOLE_GAME_GAME_H
---
+++ src/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Austin Roa on 7/26/18.
+//
+
+#include "../headers/Game.h"
